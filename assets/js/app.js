d6984f4a348cbe2fe66016b2f6975b5a5097877a
Yana Kozarenko <event2016.2017@gmail.com>
2023-05-20 20:59:18 +0100

    add cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,36 @@
 let nameExercise = document.querySelector("#nameExercise");
 let buttonNameExercise = document.querySelector("#buttonNameExercise");
+let resultNameExercise = document.querySelector("#resultNameExercise");
+
+let yourExercises = [];
+let newExercise;
+
+let checkboxTypeExercise = document.querySelectorAll(
+  'input[name="typeOfExercises"]:checked'
+);
+
 let apiKey = "e2tk2aVVqC7JgZSqYZMu0w==XUAmhHQElchlx6Fi";
 
-let yourExerciseArray = [];
+// Create Exersise Card
+
+/*let cardExercise = (
+  name,
+  type,
+  muscle,
+  equipment,
+  difficulty,
+  instructions
+) => {
+  this.name = name;
+  this.type = type;
+  this.muscle = muscle;
+  this.equipment = equipment;
+  this.difficulty = difficulty;
+  this.instructions = instructions;
+};
+cardExercise.prototype.read = false; */
+
+// Get Exercises For The Input from Searchbar
 
 const fetchExerciseByName = () => {
   let name = `${nameExercise.value}`;
@@ -14,8 +42,30 @@ const fetchExerciseByName = () => {
     .then((res) => res.json())
     .then((result) => {
       console.log(result);
-      yourExerciseArray.push(result);
-      console.log(yourExerciseArray);
+      /*resultNameExercise.innerHTML = JSON.stringify(result);
+      yourExercises.push(result);
+      console.log(yourExercises);*/
+      result.map((value) => {
+        for (let property in value) {
+          let cardArticle = document.createElement("article");
+          cardArticle.innerHTML = `
+                              <div class="exerciseCard">
+                                <h4 class="nameExercise">${value.name}</h4>
+                                <p class="typeExercise">${value.type}</p>
+                                <p class="muscleExercise">${value.muscle}</p>
+                                <p class="equipmentExercise">${value.equipment}</p>
+                                <p class="difficultyExercise">${value.difficulty}</p>
+                                <p class="instructionsExercise">${value.instructions}</p>
+                              </div> 
+                              <button class="info-button">+ info</button>`;
+
+          document
+            .querySelector("#resultNameExercise")
+            .appendChild(cardArticle);
+
+          return `${property}: ${value[property]}`;
+        }
+      });
     })
 
     .catch((error) => console.log(error));
@@ -27,6 +77,8 @@ buttonNameExerciseHandler = (event) => {
 };
 
 buttonNameExercise.addEventListener("click", buttonNameExerciseHandler);
+
+// Get Exercises For The Input Checkbox Values
 
 /*
 const fetchExerciseByName = () => {
